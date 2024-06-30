5b57648992f10106280a0f636c1932824cd7f6c0
halen-55 <halen-55@mail.ru>
2024-07-01 01:53:22 +0600

    Yandex map is configured and stylized

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Nav icon
+/* Nav icon */
 const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
 const nav = document.querySelector('.header__top-row');
@@ -9,11 +9,10 @@ navBtn.onclick = function () {
     document.body.classList.toggle('no-scroll');
 }
 
-// Phone mask
+/* Phone Mask */
 mask('[data-tel-input]');
 
-// Delete plus
-
+// Удаляем '+' если больше ничего не введено, чтобы показать placeholder
 const phoneInputs = document.querySelectorAll('[data-tel-input]');
 phoneInputs.forEach((input)=>{
 	input.addEventListener('input', ()=>{
@@ -23,3 +22,56 @@ phoneInputs.forEach((input)=>{
 		if (input.value == '+') input.value = '';
 	})
 });
+
+/* Yandex Map */
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init(){
+	// Создание карты.
+	var map = new ymaps.Map('map', {
+		// Координаты центра карты.
+		// Порядок по умолчанию: «широта, долгота».
+		// Чтобы не определять координаты центра карты вручную,
+		// воспользуйтесь инструментом Определение координат.
+		center: [59.943543, 30.338928],
+		// Уровень масштабирования. Допустимые значения:
+		// от 0 (весь мир) до 19.
+		zoom: 16,
+	});
+
+	var myPlacemark = new ymaps.Placemark(
+		[59.943543, 30.338928],
+		{
+			balloonContent: `
+				<div class="balloon">
+					<div class="balloon__address">Наб. реки Фонтанки 10-15</div>
+					<div class="balloon__contacts">
+						<a href="tel:+78121234567">+8 (812) 123-45-67</a>
+					</div>
+				</div>
+			`,
+		},
+		{
+			iconLayout: 'default#image',
+			iconImageHref: './img/map/location-pin.svg',
+			iconImageSize: [40, 40],
+			iconImageOffset: [-20, -40],
+		}
+	);
+
+	map.controls.remove('geolocationControl'); // удаляем геолокацию
+	map.controls.remove('searchControl'); // удаляем поиск
+	map.controls.remove('trafficControl'); // удаляем контроль трафика
+	map.controls.remove('typeSelector'); // удаляем тип
+
+	// map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+	// map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+	map.controls.remove('rulerControl'); // удаляем контрол правил
+	map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+	map.geoObjects.add(myPlacemark);
+    myPlacemark.balloon.open();
+
+}
